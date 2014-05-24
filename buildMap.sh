04fa32a9481b2fdf1ac9c57df3495c58ab0a122b
darkf <lw9k123@gmail.com>
2014-05-24 02:30:42 -0700

    convertObjects: add arguments to not overwrite existing files

--- buildMap.sh
+++ buildMap.sh
@@ -1,3 +1,3 @@
 baseFile=$(basename ${1%.*}) # strip extension
 python fo2map.py $1
-python frmOffsetTable.py $baseFile.images.txt > $baseFile.offsets.json
+python frmOffsetTable.py $baseFile.images.txt | python buildImageInfo.py > $baseFile.offsets.json
--- convertObjects.py
+++ convertObjects.py
@@ -1,12 +1,10 @@
 import sys, os, glob
 
-# todo: add options for not exporting .BMPs that exist, etc
-
 DATA_PATH = "data"
 OUT_DIR = ""
 
-if len(sys.argv) != 2:
-	print "USAGE: %s <object_list.txt>" % sys.argv[0]
+if len(sys.argv) < 2:
+	print "USAGE: %s <object_list.txt> [--no-extract] [--no-overwrite]" % sys.argv[0]
 	sys.exit(1)
 
 FRM2BMP_PATH = os.path.abspath("frm2bmp.exe")
@@ -48,8 +46,12 @@ for tile in tiles:
 
 for path,basedir in paths:
 	os.chdir(os.path.join(dataPath, os.path.dirname(path)))
-	#os.system(FRM2BMP_PATH + " " + os.path.basename(path))
 	img = os.path.splitext(os.path.basename(path))[0]
+	if '--no-extract' not in sys.argv:
+		if len(glob.glob(img + "*.bmp")) > 0 and '--no-overwrite' in sys.argv:
+			pass
+		else:
+			os.system(FRM2BMP_PATH + " " + os.path.basename(path))
 	bmps = glob.glob(img + "*.bmp")
 	bmps_multiframe = [bmp for bmp in bmps if '_' in bmp]
 	if len(bmps_multiframe) == 0:
@@ -58,12 +60,16 @@ for path,basedir in paths:
 		basename = os.path.splitext(bmp)[0]
 		filename = os.path.join(basedir, basename) # e.g. art/tiles/BRICK23_000.BMP
 		out = os.path.abspath(os.path.join(basePath, filename + ".png"))
+		if os.path.exists(out) and '--no-overwrite' in sys.argv:
+			continue
 		os.system("gm convert -type TrueColor -transparent \"rgb(11,0,11)\" " + bmp + " " + out)
 	elif len(bmps_multiframe) > 0:
 		# animations (create a spritesheet)
 		basename = os.path.splitext(bmps[0].split("_")[0])[0]
 		filename = os.path.join(basedir, basename) # e.g. art/tiles/BRICK23_000.BMP
 		out = os.path.abspath(os.path.join(basePath, filename + ".png"))
+		if os.path.exists(out) and '--no-overwrite' in sys.argv:
+			continue
 		print filename
 		os.system("gm montage " + basename + "_*.bmp -type TrueColor -transparent \"rgb(11,0,11)\" -background transparent -geometry +0+0 -tile 99999x1 " + out)
 	else:
