6af8542f3c2686f68229f6db12e72c26004a482e
darkf <lw9k123@gmail.com>
2014-06-13 02:03:03 -0700

    display inventory

--- exportImages.py
+++ exportImages.py
@@ -16,7 +16,7 @@ def main():
 
 	palette = pal.readPAL(open(PALETTE, "rb"))
 
-	subdirs = ("tiles",) #("critters", "items", "scenery", "walls", "misc") # etc
+	subdirs = ("inven",)#("tiles",) #("critters", "items", "scenery", "walls", "misc") # etc
 	imageInfo = {}
 
 	for subdir in subdirs:
