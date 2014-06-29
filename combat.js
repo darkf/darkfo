d424bc7d16aec7b78a5cce295cc13ac4c284cf03
Stratege <github@christian-fania.de>
2014-06-29 18:33:38 +0200

    manual merging because git got a bit confused

--- combat.js
+++ combat.js
@@ -57,39 +57,52 @@ ActionPoints.prototype.substractCombatAP = function(value) {
 	}
 }
 
+var AI = function(combatant) {
+	this.combatant = combatant
+
+	if(AI.aiTxt === null) { // load AI.TXT
+		AI.aiTxt = {}
+		var ini = parseIni(getFileText("data/data/AI.TXT"))
+		if(ini === null) throw "couldn't load AI.TXT"
+		for(var key in ini) {
+			ini[key].keyName = key
+			AI.aiTxt[ini[key].packet_num] = ini[key]
+		}
+	}
+
+	this.info = AI.aiTxt[this.combatant.pro.extra.AI]
+	if(this.info === undefined)
+		throw "no AI packet for " + combatant.toString() +
+			  " (packet " + this.combatant.pro.extra.AI + ")"
+}
+
+AI.aiTxt = null // AI.TXT: packet num -> key/value
+
+
 
 var Combat = function(objects, player) {
-	this.critters = []
+	this.combatants = []
 	for(var i = 0; i < objects.length; i++) {
 		if(objects[i].type === "critter") {
-			this.critters.push(objects[i])
+			this.combatants.push(objects[i])
 
 			if(objects[i].stats === undefined)
-				throw "no stats"; //objects[i].stats = this.getDefaultStats(objects[i])
+				throw "no stats"
 			objects[i].dead = false
 		}
 	}
 
-	//player is now just another critter
-	this.critters.push(player)
-	
-	
 	this.AP = new Array(this.critters.length)
-	
+	this.player = player
 	this.turnNum = 0
 	this.whoseTurn = -2
 	this.inPlayerTurn = false
 }
 
-Combat.prototype.fireDistance = function(obj) {
-	return obj.leftHand.attackOne['maxRange'] //todo: implement proper attack selection
+Combat.prototype.log = function(msg) {
+	// Combat-related debug log
+	console.log(msg)
 }
-
-Combat.prototype.getDefaultStats = function(obj) {
-	return {STR: 4, PER: 5, END: 5, CHR: 1, INT: 1, AGI: 1, LUK: 1, HP: 100}
-}
-
-
 
 Combat.prototype.getRandomInt = function(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min
@@ -126,8 +139,9 @@ Combat.prototype.attack = function(obj, target, callback) {
 	critterStaticAnim(obj, "attack", callback)
 
 	var damage = this.getDamageDone(obj, target)
-	var who = obj.isPlayer ? "You" : obj.name //Todo: change to NPC name
-	console.log(who + " hit the target for " + damage + " damage")
+	var who = obj.isPlayer ? "You" : critterGetName(obj)
+	var targetName = target.isPlayer ? "you" : critterGetName(target)
+	this.log(who + " hit " + targetName + " for " + damage + " damage")
 	target.stats.HP -= damage
 
 	if(target.stats.HP <= 0) {
@@ -143,29 +157,74 @@ Combat.prototype.attack = function(obj, target, callback) {
 	}
 }
 
+
+Combat.prototype.getCombatAIMessage = function(id) {
+	return getMessage("combatai", id)
+}
+
+Combat.prototype.maybeTaunt = function(obj, type, roll) {
+	if(roll === false) return
+	var msgID = this.getRandomInt(parseInt(obj.ai.info[type+"_start"]),
+	                              parseInt(obj.ai.info[type+"_end"]))
+	this.log("[TAUNT " + critterGetName(obj) + ": " + this.getCombatAIMessage(msgID) + "]")
+}
+
+Combat.prototype.findTarget = function(obj) {
+	// todo: find target according to AI rules
+	return this.player
+}
+
+
+Combat.prototype.walkUpTo = function(obj, idx, target, maxDistance, callback) {
+	// Walk up to `maxDistance` hexes, adjusting AP to fit
+	if(critterWalkTo(obj, target, false, callback, maxDistance) !== false) {
+		// OK
+		this.AP[idx] -= obj.path.path.length
+		return true
+	}
+
+	return false
+}
+
 Combat.prototype.doAITurn = function(obj, idx) {
 	var that = this
+	var target = this.findTarget(obj)	
 	var distance = hexDistance(obj.position, this.player.position)
 	var AP = this.AP[idx]
+	var messageRoll = obj.ai.info.chance <= this.getRandomInt(0, 100)	
 
 	if(this.getAvailableMoveAP(AP) <= 0) { // out of AP
-		this.nextTurn()
-		return
+		return this.nextTurn()
 	}
 
 	// behaviors
-
-	var fireDistance = this.fireDistance(obj)
+	if(obj.stats.HP <= obj.ai.info.min_hp) { // hp <= min fleeing hp, so flee
+		this.log("[AI FLEES]")
+		// todo: pick the closest edge of the map
+		this.maybeTaunt(obj, "run", messageRoll)
+		var targetPos = {x: 128, y: obj.position.y} // left edge
+		this.walkUpTo(obj, idx, targetPos, AP, function() {
+			critterStopWalking(obj)
+			that.doAITurn(obj, idx) // if we can, do another turn
+		})
+		return
+	}
+	
+	var weapon = critterGetEquippedWeapon(obj)
+	var fireDistance = weapon.getMaximumRange(1)
+	
+	// are we not in firing distance?
 	if(distance > fireDistance) {
 		// todo: some sane direction, and also path checking
-		console.log("[AI CREEPS]")
-		var neighbors = hexNeighbors(this.player.position)
-		var maxDistance = Math.min(this.AP[idx], fireDistance)
+		this.log("[AI CREEPS]")
+		var neighbors = hexNeighbors(target.position)
+		var maxDistance = Math.min(AP, fireDistance)
+		this.maybeTaunt(obj, "move", messageRoll)
 
 		for(var i = 0; i < neighbors.length; i++) {
 			if(critterWalkTo(obj, neighbors[i], false, function() {
 				critterStopWalking(obj)
-				that.doAITurn(obj, idx)
+				that.doAITurn(obj, idx) // if we can, do another turn
 			}, maxDistance) !== false) {
 				// OK
 				this.AP[idx] -= obj.path.path.length
@@ -174,27 +233,45 @@ Combat.prototype.doAITurn = function(obj, idx) {
 		}
 
 		// no path
-		console.log("[NO PATH]")
-		that.doAITurn(obj, idx)
+		this.log("[NO PATH]")
+		that.doAITurn(obj, idx) // if we can, do another turn
 	}
-	else if(AP >= 4) {
-		console.log("[ATTACKING]")
+	else if(AP >= 4) { // if we are in range, do we have enough AP to attack?
+		this.log("[ATTACKING]")
 		this.AP[idx] -= 4
 
-		if(!obj.leftHand)
-			throw "combatant has no left handed item"
+		if(critterGetEquippedWeapon(obj) === null)
+			throw "combatant has no equipped weapon"
 
-		this.attack(obj, this.player, function() {
+		this.attack(obj, target, function() {
 			critterStopWalking(obj)
-			that.doAITurn(obj, idx)
+			that.doAITurn(obj, idx) // if we can, do another turn
 		})
 	}
 	else this.nextTurn()
 }
 
+Combat.prototype.numAlive = function() {
+	var count = 0
+	for(var i = 0; i < this.combatants.length; i++) {
+		if(this.combatants[i].dead !== true)
+			count++
+	}
+	return count
+}
+
+Combat.prototype.end = function() {
+	console.log("[end combat]")
+	combat = null // todo: invert control
+	inCombat = false
+}
+
 Combat.prototype.nextTurn = function() {
+	if(this.numAlive() === 0)
+		return this.end()
+
 	this.whoseTurn++
-	if(this.whoseTurn >= this.critters.length) {
+	if(this.whoseTurn >= this.combatants.length) {
 		// end of turn
 		this.whoseTurn = -1
 	}
@@ -206,10 +283,11 @@ Combat.prototype.nextTurn = function() {
 	}
 	else {
 		this.inPlayerTurn = false
-		var critter = this.critters[this.whoseTurn]
+		var critter = this.combatants[this.whoseTurn]
 		if(critter.dead === true)
 			return this.nextTurn()
-		this.AP[this.whoseTurn].resetAP() // reset AP
+		// todo: convert unused AP into AC
+		this.AP[this.whoseTurn] = this.getMaxAP(critter) // reset AP
 		this.doAITurn(critter, this.whoseTurn)
 	}
 }
