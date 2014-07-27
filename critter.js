ce3b108f0d7a831acf38aab676b41cd0d2ef8f78
darkf <lw9k123@gmail.com>
2014-07-27 16:53:46 -0700

    Relicense, with permission from authors, under the terms of the Apache License, Version 2.0

--- critter.js
+++ critter.js
@@ -1,6 +1,20 @@
-// DarkFO
-// Copyright (c) 2014 darkf
-// Licensed under the terms of the zlib license
+/*
+Copyright 2014 darkf, Stratege
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Collection of functions for dealing with critters
 
 // TODO: Critter should really be a class of its own
 
@@ -32,7 +46,7 @@ var damageType = {'normal': 0, 'laser': 1, 'fire': 2, 'plasma': 3,
 
 var weaponSkillMap = {'uzi': 'Small Guns',
                       'rifle': 'Small Guns',
-	                  'spear': 'Melee Weapons'}
+                      'spear': 'Melee Weapons'}
 			
 function parseAttack(weapon) {
 	var attackModes = weapon.pro.extra['attackMode']
