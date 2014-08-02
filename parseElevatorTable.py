841e96d10551026e33164649ce859d7853f38a81
darkf <lw9k123@gmail.com>
2014-08-01 22:58:03 -0700

    parseElevatorTable: fix: parse unused buttons

--- parseElevatorTable.py
+++ parseElevatorTable.py
@@ -19,6 +19,7 @@ limitations under the License.
 import sys, os, struct, json
 
 NUM_ELEVATORS = 24 # there are 24 total elevators
+NUM_BUTTONS = 4 # max number of buttons
 
 # for Fallout 2 English v1.02d
 GRPH_ID = 0x0002ED50 # General image stuff
@@ -57,10 +58,14 @@ def parseElevators(f):
 	for i in range(NUM_ELEVATORS):
 		btnCount = elevators[i]['buttonCount']
 		elevators[i]['buttons'] = [{} for _ in range(btnCount)]
-		for btn in range(btnCount):
-			elevators[i]['buttons'][btn]['mapID'] = read32(f)
-			elevators[i]['buttons'][btn]['level'] = read32(f)
-			elevators[i]['buttons'][btn]['tileNum'] = read32(f)
+		for btn in range(NUM_BUTTONS):
+			if btn > btnCount-1:
+				# ignore unused buttons
+				read32(f); read32(f); read32(f)
+			else:
+				elevators[i]['buttons'][btn]['mapID'] = read32(f)
+				elevators[i]['buttons'][btn]['level'] = read32(f)
+				elevators[i]['buttons'][btn]['tileNum'] = read32(f)
 
 	for i in range(NUM_ELEVATORS):
 		print "elevator", i
