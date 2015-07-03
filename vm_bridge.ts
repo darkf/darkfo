d2aae2c436e0ab33e02c0e9c5a3ff48c2fa6943c
darkf <lw9k123@gmail.com>
2015-07-03 05:54:59 -0700

    vm_bridge: implement many operations

--- vm_bridge.ts
+++ vm_bridge.ts
@@ -32,12 +32,37 @@ module ScriptVMBridge {
 	var bridgeOpMap = {
 	    0x80BF: function() { this.push(player) } // dude_obj
 	   ,0x80BC: function() { this.push(this.scriptObj.self_obj) } // self_obj
+       ,0x8128: function() { this.push(this.scriptObj.combat_is_initialized) } // combat_is_initialized
+       ,0x8118: function() { this.push(1) } // get_month // TODO
+       ,0x80F6: function() { this.push(1200) } // game_time_hour // TODO
+
        ,0x80B4: bridged("random", 2)
        ,0x80CA: bridged("get_critter_stat", 2)
        ,0x8105: bridged("message_str", 2)
        ,0x80B8: bridged("display_msg", 1)
        ,0x810E: bridged("reg_anim_func", 2)
        ,0x8126: bridged("reg_anim_animate_forever", 2)
+       ,0x810B: bridged("metarule", 2)
+       ,0x80C1: bridged("local_var", 1)
+       ,0x80C5: bridged("global_var", 1)
+       ,0x80C6: bridged("set_global_var", 2)
+       ,0x80C2: bridged("set_local_var", 2)
+       ,0x80B7: bridged("create_object_sid", 4)
+       ,0x8102: bridged("critter_add_trait", 4)
+       ,0x8116: bridged("add_mult_objs_to_inven", 3)
+       ,0x80DC: bridged("obj_can_see_obj", 2)
+       ,0x80E9: bridged("set_light_level", 1)
+       ,0x80BB: bridged("tile_contains_obj_pid", 3)
+       ,0x80A9: bridged("override_map_start", 4)
+       ,0x8154: bridged("debug_msg", 1)
+       ,0x80F3: bridged("has_trait", 3)
+       ,0x8149: bridged("obj_art_fid", 1)
+       ,0x80DE: bridged("start_gdialog", 5)
+       ,0x811C: bridged("gsay_start", 0)
+       ,0x811E: bridged("gsay_reply", 2)
+       ,0x8121: bridged("giq_option", 5) // TODO: wrap this so that target becomes a function
+
+
     }
 
     // update VM opMap with our bridgeOpMap
@@ -53,8 +78,10 @@ module ScriptVMBridge {
 
     	    // patch scriptObj to allow transparent procedure calls
     	    // TODO: maybe we should check if we're interrupting the VM
-    	    for(var procName in this.intfile.procedures) {
-    	    	this.scriptObj[procName] = () => { this.call(procName) }
+    	    for(var _procName in this.intfile.procedures) {
+    	    	(procName => {
+	    	    	this.scriptObj[procName] = () => { this.call(procName) }
+	    	    })(_procName)
     	    }
     	}
     }
