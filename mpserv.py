3a39e164e33a930e809a1b481ce06309d62d563e
darkf <lw9k123@gmail.com>
2017-11-25 15:27:45 -0600

    mpserv: Add license header

--- mpserv.py
+++ mpserv.py
@@ -1,3 +1,21 @@
+"""
+Copyright 2017 darkf
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# WebSocket server for DarkFO's multiplayer mode
+
 from eventlet import wsgi, websocket
 import eventlet
 import json, zlib, time, string, os
@@ -8,19 +26,6 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 def is_valid_name_char(c):
     return c in string.ascii_letters + string.digits + "-_"
-
-"""
-sessions = {}
-
-def broadcast_to_user_sessions(username, t, msg):
-    global sessions
-
-    for session in user_sessions(username):
-        try:
-            session.send(t, msg)
-        except OSError:
-            session.disconnected("Broken pipe")
-"""
 
 class GameContext:
     def __init__(self):
