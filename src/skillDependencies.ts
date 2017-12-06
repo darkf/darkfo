e3c66fe49cb95581d4fcfd25c6c5bd1b0e18e06b
darkf <lw9k123@gmail.com>
2017-12-06 00:19:36 -0600

    skillDependencies: cleanup

--- src/skillDependencies.ts
+++ src/skillDependencies.ts
@@ -22,96 +22,97 @@ limitations under the License.
 enum StatType { STR, PER, END, CHR, INT, AGI, LCK, One }
 
 class Skill {
-	constructor(public startValue: number, public dependencies: Dependency[]) {
-	}
+    constructor(public startValue: number, public dependencies: Dependency[]) {
+    }
 }
 
 class Dependency {
-	statType: string;
-	multiplicator: number;
+    statType: string;
+    multiplicator: number;
 
-	constructor(_statType, _multiplicator) {
-		this.statType = _statType
-		this.multiplicator = _multiplicator
-	}
+    constructor(_statType, _multiplicator) {
+        this.statType = _statType
+        this.multiplicator = _multiplicator
+    }
 }
 
 class Stat {
-	constructor(public min: number, public max: number, public defaultValue: number, public dependencies: Dependency[]) {
-	}
+    constructor(public min: number, public max: number, public defaultValue: number, public dependencies: Dependency[]) {
+    }
 }
 
 // Skills
 
 // Fallout 2 specific, FO1 uses its own, possibly extracting this to an outside file that is loaded in would thus make sense
-var skillDependencies: { [name: string]: Skill } = {};
-skillDependencies['Small Guns'] = new Skill(5, [new Dependency('AGI', 4)])
-skillDependencies['Big Guns'] = new Skill(0, [new Dependency('AGI', 2)])
-skillDependencies['Energy Weapons'] = new Skill(0, [new Dependency('AGI',2)])
-skillDependencies['Unarmed'] = new Skill(30, [new Dependency('AGI', 2), new Dependency('STR',2)])
-skillDependencies['Melee Weapons'] = new Skill(20, [new Dependency('AGI',2), new Dependency('STR',2)])
-skillDependencies['Throwing'] = new Skill(0, [new Dependency('AGI',4)])
-skillDependencies['First Aid'] = new Skill(0, [new Dependency('PER',2), new Dependency('INT',2)])
-skillDependencies['Doctor'] = new Skill(5, [new Dependency('PER',1), new Dependency('INT',1)])
-skillDependencies['Sneak'] = new Skill(5, [new Dependency('AGI',3)])
-skillDependencies['Lockpick'] = new Skill(10, [new Dependency('PER',1), new Dependency('AGI',1)])
-skillDependencies['Steal'] = new Skill(0, [new Dependency('AGI',3)])
-skillDependencies['Traps'] = new Skill(10, [new Dependency('PER',1), new Dependency('AGI',1)])
-skillDependencies['Science'] = new Skill(0, [new Dependency('INT',4)])
-skillDependencies['Repair'] = new Skill(0, [new Dependency('INT',3)])
-skillDependencies['Speech'] = new Skill(0, [new Dependency('CHA',5)])
-skillDependencies['Barter'] = new Skill(0, [new Dependency('CHA',4)])
-skillDependencies['Gambling'] = new Skill(5, [new Dependency('LUK',5)])
-skillDependencies['Outdoorsman'] = new Skill(0, [new Dependency('END',2), new Dependency('INT',2)])
+const skillDependencies: { [name: string]: Skill } = {
+    "Small Guns": new Skill(5, [new Dependency("AGI", 4)]),
+    "Big Guns": new Skill(0, [new Dependency("AGI", 2)]),
+    "Energy Weapons": new Skill(0, [new Dependency("AGI", 2)]),
+    "Unarmed": new Skill(30, [new Dependency("AGI", 2), new Dependency("STR", 2)]),
+    "Melee Weapons": new Skill(20, [new Dependency("AGI", 2), new Dependency("STR", 2)]),
+    "Throwing": new Skill(0, [new Dependency("AGI", 4)]),
+    "First Aid": new Skill(0, [new Dependency("PER", 2), new Dependency("INT", 2)]),
+    "Doctor": new Skill(5, [new Dependency("PER", 1), new Dependency("INT", 1)]),
+    "Sneak": new Skill(5, [new Dependency("AGI", 3)]),
+    "Lockpick": new Skill(10, [new Dependency("PER",1), new Dependency("AGI", 1)]),
+    "Steal": new Skill(0, [new Dependency("AGI", 3)]),
+    "Traps": new Skill(10, [new Dependency("PER", 1), new Dependency("AGI", 1)]),
+    "Science": new Skill(0, [new Dependency("INT", 4)]),
+    "Repair": new Skill(0, [new Dependency("INT", 3)]),
+    "Speech": new Skill(0, [new Dependency("CHA", 5)]),
+    "Barter": new Skill(0, [new Dependency("CHA", 4)]),
+    "Gambling": new Skill(5, [new Dependency("LUK", 5)]),
+    "Outdoorsman": new Skill(0, [new Dependency("END", 2), new Dependency("INT", 2)]),
+};
 
 // Stats
 
-var statDependencies: { [name: string]: Stat } = {};
-statDependencies['STR'] = new Stat(1, 10, 5, [])
-statDependencies['PER'] = new Stat(1, 10, 5, [])
-statDependencies['END'] = new Stat(1, 10, 5, [])
-statDependencies['CHA'] = new Stat(1, 10, 5, [])
-statDependencies['INT'] = new Stat(1, 10, 5, [])
-statDependencies['AGI'] = new Stat(1, 10, 5, [])
-statDependencies['LUK'] = new Stat(1, 10, 5, [])
+const statDependencies: { [name: string]: Stat } = {
+    "STR": new Stat(1, 10, 5, []),
+    "PER": new Stat(1, 10, 5, []),
+    "END": new Stat(1, 10, 5, []),
+    "CHA": new Stat(1, 10, 5, []),
+    "INT": new Stat(1, 10, 5, []),
+    "AGI": new Stat(1, 10, 5, []),
+    "LUK": new Stat(1, 10, 5, []),
 
-statDependencies['Max HP'] = new Stat(0, 999, 0, [new Dependency('One', 15), new Dependency('END', 2), new Dependency('STR', 2)])
-statDependencies['AP'] = new Stat(1, 99, 0, [new Dependency('One', 5), new Dependency('AGI', 0.5)])
-statDependencies['AC'] = new Stat(0, 999, 0, [new Dependency('AGI', 1)])
-statDependencies['Melee'] = new Stat(1, 500, 0, [new Dependency('One', -5), new Dependency('STR', 1)])
-statDependencies['Carry'] = new Stat(0, 999, 0, [new Dependency('One', 25), new Dependency('STR', 25)])
-statDependencies['Sequence'] = new Stat(0, 60, 0, [new Dependency('PER', 2)])
-statDependencies['Healing Rate'] = new Stat(1, 30, 0, [new Dependency('END', 1/3)])
-statDependencies['Critical Chance'] = new Stat(0, 100, 0, [new Dependency('LUK',1)])
-statDependencies['Better Criticals'] = new Stat(-60, 100, 0,[])
-statDependencies['DT EMP'] = new Stat(0, 100, 0, [])
-statDependencies['DT Electrical'] = new Stat(0, 100, 0, [])
-statDependencies['DT Explosive'] = new Stat(0, 100, 0, [])
-statDependencies['DT Fire'] = new Stat(0, 100, 0, [])
-statDependencies['DT Laser'] = new Stat(0, 100, 0, [])
-statDependencies['DT Normal'] = new Stat(0, 100, 0, [])
-statDependencies['DT Plasma'] = new Stat(0, 100, 0, [])
-statDependencies['DR EMP'] = new Stat(0, 100, 0, [])
-statDependencies['DR Electrical'] = new Stat(0, 90, 0, [])
-statDependencies['DR Explosive'] = new Stat(0, 90,0,[])
-statDependencies['DR Fire'] = new Stat(0, 90, 0, [])
-statDependencies['DR Laser'] = new Stat(0, 90, 0, [])
-statDependencies['DR Normal'] = new Stat(0, 90, 0, [])
-statDependencies['DR Plasma'] = new Stat(0, 90, 0, [])
-statDependencies['DR Radiation'] = new Stat(0, 95, 0, [new Dependency('END', 2)])
-statDependencies['DR Poison'] = new Stat(0, 95, 0, [new Dependency('END', 5)])
-statDependencies['Age'] = new Stat(16, 101, 25, [])
-statDependencies['Gender'] = new Stat(0, 1, 0, [])
-//todo: figure out HP.
-statDependencies['HP'] = new Stat(0, 999, 1, [])
-statDependencies['Poison Level'] = new Stat(0, 2000, 0, [])
-statDependencies['Radiation Level'] = new Stat(0, 2000, 0, [])
-statDependencies['Skill Points'] = new Stat(0, 999999, 0, [])
-statDependencies['Level'] = new Stat(1, 99, 1, [])
-statDependencies['Experience'] = new Stat(0, 99999999, 0, [])
-statDependencies['Reputation'] = new Stat(-20, 20, 0, [])
-statDependencies['Karma'] = new Stat(-99999999, 99999999, 0, [])
-
+    "Max HP": new Stat(0, 999, 0, [new Dependency('One', 15), new Dependency('END', 2), new Dependency('STR', 2)]),
+    "AP": new Stat(1, 99, 0, [new Dependency('One', 5), new Dependency('AGI', 0.5)]),
+    "AC": new Stat(0, 999, 0, [new Dependency('AGI', 1)]),
+    "Melee": new Stat(1, 500, 0, [new Dependency('One', -5), new Dependency('STR', 1)]),
+    "Carry": new Stat(0, 999, 0, [new Dependency('One', 25), new Dependency('STR', 25)]),
+    "Sequence": new Stat(0, 60, 0, [new Dependency('PER', 2)]),
+    "Healing Rate": new Stat(1, 30, 0, [new Dependency('END', 1/3)]),
+    "Critical Chance": new Stat(0, 100, 0, [new Dependency('LUK', 1)]),
+    "Better Criticals": new Stat(-60, 100, 0, []),
+    "DT EMP": new Stat(0, 100, 0, []),
+    "DT Electrical": new Stat(0, 100, 0, []),
+    "DT Explosive": new Stat(0, 100, 0, []),
+    "DT Fire": new Stat(0, 100, 0, []),
+    "DT Laser": new Stat(0, 100, 0, []),
+    "DT Normal": new Stat(0, 100, 0, []),
+    "DT Plasma": new Stat(0, 100, 0, []),
+    "DR EMP": new Stat(0, 100, 0, []),
+    "DR Electrical": new Stat(0, 90, 0, []),
+    "DR Explosive": new Stat(0, 90,0,[]),
+    "DR Fire": new Stat(0, 90, 0, []),
+    "DR Laser": new Stat(0, 90, 0, []),
+    "DR Normal": new Stat(0, 90, 0, []),
+    "DR Plasma": new Stat(0, 90, 0, []),
+    "DR Radiation": new Stat(0, 95, 0, [new Dependency('END', 2)]),
+    "DR Poison": new Stat(0, 95, 0, [new Dependency('END', 5)]),
+    "Age": new Stat(16, 101, 25, []),
+    "Gender": new Stat(0, 1, 0, []),
+    //todo: figure out HP.,
+    "HP": new Stat(0, 999, 1, []),
+    "Poison Level": new Stat(0, 2000, 0, []),
+    "Radiation Level": new Stat(0, 2000, 0, []),
+    "Skill Points": new Stat(0, 999999, 0, []),
+    "Level": new Stat(1, 99, 1, []),
+    "Experience": new Stat(0, 99999999, 0, []),
+    "Reputation": new Stat(-20, 20, 0, []),
+    "Karma": new Stat(-99999999, 99999999, 0, []),
+};
 
 // TODO: figure out what is going on with Skill
 // all the weird pseudo stats
@@ -124,22 +125,22 @@ statDependencies['One'] = new Stat(1, 1, 1, [])
 
 //FO2 specific, in FO1 it's always 1
 function getImprovementCost(obj, skill) {
-	var skillPoints = critterGetSkill(obj, skill)
-	if(skillPoints == null)
-		return null
-	
-	if(skillPoints<101)
-		return 1
-	else if(skillPoints<126)
-		return 2
-	else if(skillPoints<151)
-		return 3
-	else if(skillPoints<176)
-		return 4
-	else if(skillPoints<201)
-		return 5
-	else if(skillPoints<301)
-		return 6
-	else
-		return 999999999
+    var skillPoints = critterGetSkill(obj, skill)
+    if(skillPoints == null)
+        return null
+    
+    if(skillPoints<101)
+        return 1
+    else if(skillPoints<126)
+        return 2
+    else if(skillPoints<151)
+        return 3
+    else if(skillPoints<176)
+        return 4
+    else if(skillPoints<201)
+        return 5
+    else if(skillPoints<301)
+        return 6
+    else
+        return 999999999
 }
