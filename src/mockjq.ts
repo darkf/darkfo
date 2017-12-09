8e8941a3b539c59b4488c87ef1c7b095f0213e29
darkf <lw9k123@gmail.com>
2017-12-09 01:58:10 -0600

    mockjq: Introduce base DOM event types, impl more

--- src/mockjq.ts
+++ src/mockjq.ts
@@ -5,13 +5,21 @@ interface Jq {
     css(prop: string, value: string|number): this;
     css(props: { [prop: string]: string|number }): this;
 
-    click(_: (e?: JqEvent) => void): this;
+    html(value: string): this;
+    append(value: Jq|string): this;
+
+    attr(attrib: string, value: any): this;
+
+    on<T extends Event>(event: string, handler: (e?: JqEvent<T>) => void): this;
+    click<T extends Event>(_: (e?: JqEvent<T>) => void): this;
     
     scrollLeft(_: number): this;
     scrollTop(_: number): this;
 }
 
-interface JqEvent {
+interface JqEvent<EventType extends Event> {
+    originalEvent: EventType;
+
     pageX: number;
     pageY: number;
     stopPropagation(): void;
