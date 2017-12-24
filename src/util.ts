ffc7adc4d9c2257ccb55873cb5a1a82321db2e56
darkf <lw9k123@gmail.com>
2017-12-24 02:11:28 -0600

    util: Import jQuery's .extend for deep cloning

--- src/util.ts
+++ src/util.ts
@@ -197,3 +197,97 @@ function assertEq<T>(value: T, expected: T, message: string) {
 	if(value !== expected)
 		throw `AssertionError: value (${value}) does not match expected (${expected}): ${message}`
 }
+
+function jQuery_isPlainObject(obj: any): boolean {
+	var proto, Ctor;
+
+	// Detect obvious negatives
+	// Use toString instead of jQuery.type to catch host objects
+	if ( !obj || toString.call( obj ) !== "[object Object]" ) {
+		return false;
+	}
+
+	proto = Object.getPrototypeOf( obj );
+
+	// Objects with no prototype (e.g., `Object.create( null )`) are plain
+	if ( !proto ) {
+		return true;
+	}
+
+	// Objects with prototype are plain iff they were constructed by a global Object function
+	Ctor = Object.hasOwnProperty.call( proto, "constructor" ) && proto.constructor;
+	return typeof Ctor === "function" && Object.toString.call( Ctor ) === Object.toString.call(Object);
+}
+
+function jQuery_extend(deep: boolean, target: any, obj: any): any {
+	var options, name, src, copy, copyIsArray, clone,
+		target = arguments[ 0 ] || {},
+		i = 1,
+		length = arguments.length,
+		deep = false;
+
+	// Handle a deep copy situation
+	if ( typeof target === "boolean" ) {
+		deep = target;
+
+		// Skip the boolean and the target
+		target = arguments[ i ] || {};
+		i++;
+	}
+
+	// Handle case when target is a string or something (possible in deep copy)
+	if ( typeof target !== "object" && typeof(target) !== "function") {
+		target = {};
+	}
+
+	// Extend jQuery itself if only one argument is passed
+	if ( i === length ) {
+		target = this;
+		i--;
+	}
+
+	for ( ; i < length; i++ ) {
+
+		// Only deal with non-null/undefined values
+		if ( ( options = arguments[ i ] ) != null ) {
+
+			// Extend the base object
+			for ( name in options ) {
+				src = target[ name ];
+				copy = options[ name ];
+
+				// Prevent never-ending loop
+				if ( target === copy ) {
+					continue;
+				}
+
+				// Recurse if we're merging plain objects or arrays
+				if ( deep && copy && ( jQuery_isPlainObject( copy ) ||
+					( copyIsArray = Array.isArray( copy ) ) ) ) {
+
+					if ( copyIsArray ) {
+						copyIsArray = false;
+						clone = src && Array.isArray( src ) ? src : [];
+
+					} else {
+						clone = src && jQuery_isPlainObject( src ) ? src : {};
+					}
+
+					// Never move original objects, clone them
+					target[ name ] = jQuery_extend( deep, clone, copy );
+
+				// Don't bring in undefined values
+				} else if ( copy !== undefined ) {
+					target[ name ] = copy;
+				}
+			}
+		}
+	}
+
+	// Return the modified object
+	return target;
+};
+
+function deepClone<T>(obj: T): T {
+	return jQuery_extend(true, {}, obj);
+}
